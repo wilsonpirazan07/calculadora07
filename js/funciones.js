3b7de8693c0269b8351449e8824e3c336128f55a
wilsonpirazan <wilson2435pirazan@gmail.com>
2023-04-27 16:01:26 -0500

    calculadora final

--- js/funciones.js
+++ js/funciones.js
@@ -1,41 +1,78 @@
 var ca = {
-    teclas: document.querySelectorAll("#calcular ul li"),
-    accion: null,
-    digito: null,
-    operacion: document.querySelector("#operaciones"),
-    resultado: false,
-    cantidecimal: false,
-    cantisignos: 0
+  teclas: document.querySelectorAll("#calcular ul li"),
+  operaciones: document.querySelector("#operaciones"),
+  resultado: false,
+  accion: null,
+  digito: null,
+  cantisignos: 0,
+  cantidecimal: false,
 };
 
-var me = {
-    inicio: function() {
-        for (var i = 0; i < ca.teclas.length; i++) {
-            ca.teclas[i].addEventListener("click", me.oprimirteclas);
-        }
-    },
-
-    oprimirteclas: function(evento) {
-        ca.accion = evento.target.getAttribute("class");
-        ca.digito = evento.target.innerHTML;
-        me.calculadora(ca.accion, ca.digito);
-    },
-
-    calculadora: function(accion, digito) {
-        ca.cantisignos = 0;
-
-        switch (accion) {
-            case "numero":
-                if (ca.operacion.innerHTML === "0") {
-                    ca.operacion.innerHTML = digito;
-                } else {
-                    ca.operacion.innerHTML += digito;
-                }
-                break;
-
-            
-        }
+var metodos = {
+  inicio: function() {
+    for(var i = 0; i < ca.teclas.length; i++) {
+      ca.teclas[i].addEventListener("click", metodos.oprimirteclas);
     }
+  },
+
+  oprimirteclas: function(evento) {
+    ca.accion = evento.target.getAttribute("class");
+    ca.digito = evento.target.innerHTML;
+    metodos.calculadora(ca.accion, ca.digito);
+  },
+
+  calculadora: function(accion, digito) {
+    switch(accion) {
+      case "numero":
+        ca.cantisignos = 0;
+        if(ca.operaciones.innerHTML === "0") {
+          ca.operaciones.innerHTML = digito;
+        } else {
+          if(ca.resultado) {
+            ca.resultado = false;
+            ca.operaciones.innerHTML = digito;
+          } else {
+            ca.operaciones.innerHTML += digito;
+          }
+        }
+        break;
+
+      case "operador":
+        ca.cantisignos++;
+        if(ca.cantisignos === 1) {
+          if(ca.operaciones.innerHTML === "0") {
+            ca.operaciones.innerHTML = "0";
+          } else {
+            ca.operaciones.innerHTML += digito;
+            ca.cantidecimal = false;
+            ca.resultado = false;
+          }
+        }
+        break;
+
+      case "decimal":
+        if(!ca.cantidecimal && ca.cantisignos !== 1) {
+          ca.operaciones.innerHTML += digito;
+          ca.cantidecimal = true;
+          ca.resultado = false;
+        }
+        break;
+
+      case "igual":
+        ca.operaciones.innerHTML = eval(ca.operaciones.innerHTML);
+        ca.resultado = true;
+        break;
+    }
+  },
+
+  borrarcalcu: function() {
+    ca.resultado = false;
+    ca.operaciones.innerHTML = "0";
+  }
 };
 
-me.inicio();
+metodos.inicio();
+   
+
+
+
